79740110424f504b8d0ffd767359a8de1b71acd0
OlesiaPonomarenko <160940182+OlesiaPonomarenko@users.noreply.github.com>
2025-12-04 12:24:18 +0200

    new

--- js.js
+++ js.js
@@ -1,25 +1,4 @@
- /*const images = document.querySelectorAll('.item');
-
-images.forEach( item => {
-    item.addEventListener('mouseover', () =>{
-    removeFocus();
-    item.classList.add('selected');
-    })
-    removeFocus = () => {
-        images.forEach(item => {
-            item.classList.remove('selected');
-        })
-    }
-})
-
-images.forEach( item => {
-    item.addEventListener ('click', () => {
-        removeFocus();
-        item.classList.add('selected');
-    })
-})
-*/
-
+ 
 const images = document.querySelectorAll('.item');
 
 function removeFocus() {
