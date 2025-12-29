7ce45ad7a56cb353c3f837174308e4755ea29cc6
OlesiaPonomarenko <160940182+OlesiaPonomarenko@users.noreply.github.com>
2025-12-29 14:17:15 +0200

    new

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
- 
+ function timerDays () {
+
+    const dateOpen = (new Date('December 31 2025 09:00'));
+    const now = new Date();
+    const diff = dateOpen - now;
+    const msInSecond = 1000;
+    const msInMinute = 60 * 1000;
+    const msInHour = 60 * 60 *1000;
+    const msInDay = 24 * 60 * 60 * 1000;
+
+    const displayDay = Math.floor(diff/msInDay);
+    document.querySelector('.days').textContent = displayDay;
+
+    const displayHours = Math.floor((diff%msInDay) / msInHour);
+    document.querySelector('.hours').textContent = displayHours;
+
+    const displayMinute = Math.floor((diff%msInHour) / msInMinute);
+    document.querySelector('.minute').textContent = displayMinute;
+
+    const displaySecond = Math.floor((diff%msInMinute) / msInSecond);
+    document.querySelector('.second').textContent = displaySecond;
+
+    if ( diff <= 0) {
+        document.querySelector('.days').textContent = 0;
+        document.querySelector('.hours').textContent = 0;
+        document.querySelector('.minute').textContent = 0;
+        document.querySelector('.second').textContent =0;
+    
+    clearInterval(timerId);
+}
+
+ }
+
+ let timerId = setInterval(timerDays, 1000);
+
+
 const images = document.querySelectorAll('.item');
 
 function removeFocus() {
